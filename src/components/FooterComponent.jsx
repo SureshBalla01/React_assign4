5540092fa93021144773d6e46cdd1cc2880fd3d4
unknown <sureshrahulkkr@gmail.com>
2022-10-21 17:28:19 +0530

    Assignment4

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,9 +1,9 @@
+import './css/Footercomponent.css';
 function FooterComponent(props){
     return (
         <div className="footer">
-            <button type="button" className="btn btn-primary">Primary</button>
             @copy; {props.copyright} <em>{props.company}</em>
-            <p>{new Date().toDateString()}</p>
+         
         </div>
     );
 }
